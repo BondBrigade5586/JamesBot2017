d0c6aeda6a6476368dd032f93a27bfc5cf566bb9
stevenott <18sott@kiel.k12.wi.us>
2017-02-21 18:50:44 -0600

    Adding Vision
    First try at adding some vision code

--- src/org/usfirst/frc/team5586/robot/Robot.java
+++ src/org/usfirst/frc/team5586/robot/Robot.java
@@ -1,102 +1,127 @@
 
 package org.usfirst.frc.team5586.robot;
 
-import java.io.IOException;
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
 
+import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Victor;
-import edu.wpi.first.wpilibj.networktables.NetworkTable;
-import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.vision.VisionThread;
+import edu.wpi.first.wpilibj.Timer;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
+
 public class Robot extends IterativeRobot {
-    final String defaultAuto = "Default";
-    final String customAuto = "My Auto";
-    String autoSelected;
-    SendableChooser chooser;
+	final String defaultAuto = "Blank";
+	final String customAuto1 = "Vision Shooting";
+	final String customAuto2 = "Vision Driving";
+	final String customAuto3 = "Move";
+	String autoSelected;
+	SendableChooser<String> chooser = new SendableChooser<>();
     Victor mFrontLeft = new Victor(0);
     Victor mBackLeft = new Victor(1);
     Victor mFrontRight = new Victor(2);
     Victor mBackRight = new Victor(3);
     RobotDrive drive = new RobotDrive(mFrontLeft, mBackLeft, mFrontRight, mBackRight);
-    Spark shooter = new Spark(4);
+    VictorSP shooter = new VictorSP(4);
+    Spark intake = new Spark(7);
+    Spark winch = new Spark(6);
     Joystick joystick = new Joystick(0);
     ADXRS450_Gyro gyro = new ADXRS450_Gyro();
-    double xSpeed;
-    double ySpeed;
-    double zSpeed;
-    boolean toggle1 = true;
-    boolean toggle2 = true;
-    boolean toggle3 = true;
-    boolean toggle4 = true;
-    boolean fullSpeed = false;
-    boolean rotation = false;
-    boolean robotCentric = false;
-    boolean shooterOn = false;
-    CameraServer server;
-    private final NetworkTable grip = NetworkTable.getTable("grip");
+    private static final int IMG_WIDTH = 320;
+	private static final int IMG_HEIGHT = 240;
+	Pipeline pipeline;
+	VisionThread visionThread;
+	private double centerX = 0.0;
+	private final Object imgLock = new Object();
+	boolean rotation;
     
     public void robotInit() {
-        chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", defaultAuto);
-        chooser.addObject("My Auto", customAuto);
-        SmartDashboard.putData("Auto choices", chooser);
-        gyro.calibrate();
-        mFrontLeft.setInverted(true);
-        mBackLeft.setInverted(true);
+        gyro.calibrate(); 
+        mFrontRight.setInverted(true);
+        mBackRight.setInverted(true);
         
+        UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+        camera.setResolution(IMG_WIDTH, IMG_HEIGHT);
         
-        server = CameraServer.getInstance();
-        server.setQuality(50);
-        server.startAutomaticCapture("cam0");
-        try {
-        	new ProcessBuilder("/home/lvuser/grip").inheritIO().start();
-        } catch (IOException e) {
-        	e.printStackTrace();
-        }
-        
-        
+        visionThread = new VisionThread(camera, new Pipeline(), pipeline -> {
+            if (!pipeline.filterContoursOutput().isEmpty()) {
+                Rect r = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
+                synchronized (imgLock) {
+                    centerX = r.x + (r.width / 2);
+                }
+            }
+        });
+        visionThread.start();
+        chooser.addDefault("Blank Auto", defaultAuto);
+		chooser.addObject("Shooting Assist", customAuto1);
+		chooser.addObject("Driving Assist", customAuto2);
+		chooser.addObject("Move Forward", customAuto3);
+		SmartDashboard.putData("Auto choices", chooser);
     }
     
     public void autonomousInit() {
-    	autoSelected = (String) chooser.getSelected();
+    	autoSelected = chooser.getSelected();
 		System.out.println("Auto selected: " + autoSelected);
+
     }
     
     public void autonomousPeriodic() {
-    	switch(autoSelected) {
-    	case customAuto:
-        //Put custom auto code here   
-            break;
-    	case defaultAuto:
-    	default:
-    	//Put default auto code here
-            break;
-    	}
+    	double centerX;
+    		switch (autoSelected) {
+    		case customAuto3: 
+    			drive.mecanumDrive_Cartesian(1, 0, 0, gyro.getAngle());
+    			Timer.delay(5);
+    			drive.mecanumDrive_Cartesian(0, 0, 0, 0);
+    			break;
+    		case customAuto2:
+        		synchronized (imgLock) {
+        			centerX = this.centerX;
+        		}
+        		double move = (centerX - (IMG_WIDTH / 2)) * 0.005;
+        		drive.mecanumDrive_Cartesian(0, move, 0, gyro.getAngle());
+    			break;
+    		case customAuto1:
+        		synchronized (imgLock) {
+        			centerX = this.centerX;
+        		}
+        		double turn = centerX - (IMG_WIDTH / 2);
+        		shooter.set(turn * 0.005);
+    			break;
+    		case defaultAuto:
+    		default:
+    			drive.mecanumDrive_Cartesian(0, 0, 0, 0);
+    			break;
+    		}
     }
 
     public void teleopPeriodic() {
-        SmartDashboard.putNumber("Gyro Angle", gyro.getAngle());
+        double xSpeed;
+        double ySpeed;
+        double zSpeed;
+        boolean toggle1 = true;
+        boolean toggle2 = true;
+        boolean toggle3 = true;
+        boolean toggle4 = true;
+        boolean toggle5 = true;
+        boolean fullSpeed = false;
+        boolean rotation = false;
+        boolean robotCentric = true;
+        boolean shooterOn = false;
+        boolean intakeOn = false;
         
-        for (double area : grip.getNumberArray("targets/area", new double[0])){
-			System.out.println("Found contour with area: " + area);
-		}
-    	
-    	// Toggle selection portion of the teleop period
-    	
+        SmartDashboard.putNumber("Gyro Angle", gyro.getAngle());
+        SmartDashboard.putBoolean("Rotation", rotation);
+        SmartDashboard.putBoolean("Robot Centric", robotCentric);
+        
     	if(toggle1 && joystick.getRawButton(8)) {
     		toggle1 = false;
     		robotCentric = !robotCentric;
@@ -104,66 +129,65 @@ public class Robot extends IterativeRobot {
     		toggle1 = true;
     	}
     	
-    	if(toggle2 && joystick.getRawButton(3)) {
+    	/* if(toggle2 && joystick.getRawButton(3)) {
     		toggle2 = false;
     		fullSpeed = !fullSpeed;
     	} else if(joystick.getRawButton(3) == false) {
     		toggle2 = true;
-    	}
+    	} */
     	
-    	if(toggle3 && joystick.getRawButton(2)) {
+    	if(toggle3 && joystick.getRawButton(9)) {
     		toggle3 = false;
     		rotation = !rotation;
-    	} else if(joystick.getRawButton(2) == false) {
+    	} else if(joystick.getRawButton(9) == false) {
     		toggle3 = true;
     	}
     	
-    	if(toggle4 && joystick.getRawButton(1)) {
-    		toggle4 = false;
-    		shooterOn = !shooterOn;
-    	} else if(joystick.getRawButton(1) == false) {
-    		toggle4 = true;
-    	}
-    	
     	//IO portion, where we get inputs and move actuators
+    	
     	if(joystick.getX() < .1 && joystick.getX() > -.1) {
     		xSpeed = 0;
     	} else {
-    		if(fullSpeed) {
-    			xSpeed = joystick.getX();
-    		} else {
-    			xSpeed = joystick.getX() * .75;
-    		}
+    		xSpeed = joystick.getX();
     	}
     	
     	if(joystick.getY() < .1 && joystick.getY() > -.1) {
     		ySpeed = 0;
     	} else {
-    		if(fullSpeed) {
-    			ySpeed = joystick.getY();
-    		} else {
-    			ySpeed = joystick.getY() * .75;
-    		}
+    		ySpeed = joystick.getY();
     	}
     	
-    	if(rotation) {
-        	zSpeed = joystick.getTwist() * .75;
-        } else {
-        	zSpeed = 0;
-        }
+    	if(joystick.getTwist() < .2 && joystick.getTwist() > -.2) {
+    		zSpeed = 0;
+    	} else {
+    		zSpeed = joystick.getTwist();
+    	}
     	
-    	if(joystick.getRawButton(1) == true) {
-    		shooter.set(.80);
+    	if(joystick.getRawButton(2) == true) {
+    		shooter.set(1);
     	} else {
     		shooter.set(0);
     	}
     	
-    	if(joystick.getRawButton(5) == true) {
+    	if(joystick.getRawButton(3) == true) {
+    		winch.set(1);
+    	} else {
+    		winch.set(0);
+    	}
+    	
+	    	if(joystick.getRawButton(1) == true) {
+	    		intake.set(1);
+	    	} else {
+    		intake.set(0);
+    	}
+    	
+    	if(joystick.getRawButton(7) == true) {
     		gyro.reset();
     	}
     	
         if(robotCentric) {
         	drive.mecanumDrive_Cartesian(xSpeed, ySpeed, zSpeed, 0);
+        	
         } else {
         	drive.mecanumDrive_Cartesian(xSpeed, ySpeed, zSpeed, gyro.getAngle());
         }
